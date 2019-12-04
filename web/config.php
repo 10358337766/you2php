b65127612a3d970ecd9678df5ebc9b0d92f18c54
10358337766 <58505460+10358337766@users.noreply.github.com>
2019-12-04 16:43:42 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -3,8 +3,8 @@ $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyCUZkk01CZlqJ61DRUDDdaUZyC4UkQvGvo');
 define('GJ_CODE', $gl);
-define('SITE_NAME', 'mumu');
-define('TITLENAME', '烂春袋');
+define('SITE_NAME', '点我点我点我');
+define('TITLENAME', '毫无意义');
 define('EN2DEKEY', 'AnQUP1XDEF8:CtzDSh4');
 define('EMAIL', 'None@gmail.com');
 ?>
